762767b7f6efc7c38a6284760c146f575dbd7851
A. Muthu Mari <72031032+MuthuMariAyyappan@users.noreply.github.com>
2024-02-04 19:02:59 +0530

    Update FloorNoInArray.java

--- Data Structure and Algorithm practice/BinarySearch/FloorNoInArray.java
+++ Data Structure and Algorithm practice/BinarySearch/FloorNoInArray.java
@@ -1,3 +1,7 @@
+ // Given a sorted array and a value x, the floor of x is the largest element in array smaller than or equal to x.
+ // Write efficient functions to find floor of x.
+ // Example: Input : arr[] = {1, 2, 8, 10, 10, 12, 19}, x = 5 Output : 2 2 is the largest element in arr[] smaller than 5.
+
 package BinarySearch;
 
 import java.util.Arrays;
